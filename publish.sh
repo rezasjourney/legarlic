2d777a32800e92054e903b9ba5ff6367d330ec44
Reza Fazeli <rezafazeli@gmail.com>
2020-08-14 21:50:37 -0400

    Update .gitignore

--- publish.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# This script cleans and updates the output/ folder and publishes it on Github Pages
-
-make clean
-pelican content -o output -s pelicanconf.py
-ghp-import output -b gh-pages
-git push git@github.com:legarlic/legarlic.github.io.git gh-pages:master
